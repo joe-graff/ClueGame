b28d67165f022461e3b8907679b72f29559742e7
Joseph Graff <jgraff@mymail.mines.edu>
2018-03-04 14:48:18 -0700

    majority of tests created, need to update with information from spreadsheet and legend

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -4,17 +4,60 @@ import java.util.Map;
 import java.util.Set;
 
 public class Board {
-	private int numRows, numColumns;
+	
+	public Board() {
+		
+	}
+	private static int NUM_ROWS;
+	private static int NUM_COLUMNS;
 	private static int MAX_BOARD_SIZE;
 	private BoardCell board[][];
-	private Map<BoardCell, Set<BoardCell>> legend;
+	private Map<Character, String> legend;
 	private String boardConfigFile;
-	private String roomConfigFile;
+	private String legendConfigFile;
 	
-	public Board getInstance() {return null;}
+	public static Board getInstance() {return null;}
 	public void initialize() {}
 	public void loadRoomConfig() {}
 	public void loadBoardConfig() {}
 	public void calcAdjacencies() {}
-	public void calcTargets(BoardCell cell, int pathLength) {};
+	public void calcTargets(BoardCell cell, int pathLength) {}
+	
+	public String getBoardConfigFile() {
+		return boardConfigFile;
+	}
+	
+	public void setBoardConfigFile(String boardConfigFile) {
+		this.boardConfigFile = boardConfigFile;
+	}
+	
+	public String getRoomConfigFile() {
+		return legendConfigFile;
+	}
+	
+	public void setLegendConfigFile(String legendConfigFile) {
+		this.legendConfigFile = legendConfigFile;
+	}
+	
+	public Map<Character, String> getLegend(){
+		return legend;
+	}
+	
+	public void setLegend(Map<Character, String> legend) {
+		this.legend = legend;
+	}
+	
+	public BoardCell getCellAt(int row, int col) {
+		BoardCell b = board[row][col];
+		return b;
+	}
+	
+	public int getRows() {
+		return NUM_ROWS;
+	}
+	
+	public int getColumns() {
+		return NUM_COLUMNS;
+	}
+	
 }
