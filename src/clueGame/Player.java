792bf8a70df649f2d53a1662e0dfc52540bf85f5
Joseph Graff <jgraff@mymail.mines.edu>
2018-04-03 15:54:53 -0600

    Refactoring files added

---
+++ src/clueGame/Player.java
@@ -0,0 +1,19 @@
+package clueGame;
+
+import java.awt.Color;
+
+/**
+ * 
+ * @author Joseph Graff and Lewis Setter
+ *
+ */
+
+public class Player {
+	private String playerName;
+	private int row;
+	private int column;
+	private Color color;
+	public Card disproveSuggestion(Solution suggestion) {
+		return null;
+	}
+}
