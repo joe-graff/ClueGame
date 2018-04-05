e789c15edbcb1b82dd416e03b5ee8cc9c7b640c3
Lewis Setter <ljsetter@mines.edu>
2018-04-04 20:32:15 -0600

    added ComputerPlayer and HumanPlayer classes, all tests green

---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,10 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class ComputerPlayer extends Player{
+
+	public ComputerPlayer(String playerName, int row, int column, Color color) {
+		super(playerName, row, column, color);
+	}
+}
---
+++ src/clueGame/HumanPlayer.java
@@ -0,0 +1,14 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class HumanPlayer extends Player {
+
+	public HumanPlayer(String playerName, int row, int column, Color color) {
+		super(playerName, row, column, color);
+	}
+	
+	
+
+}
+
