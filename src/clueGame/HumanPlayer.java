9927ffcf35af713c403ceea14c495313b862cac6
Joseph Graff <jgraff@mymail.mines.edu>
2018-04-05 15:58:21 -0600

    gameActionTests class created

---
+++ src/clueGame/HumanPlayer.java
@@ -0,0 +1,9 @@
+package clueGame;
+
+import java.awt.Color;
+
+public class HumanPlayer extends Player{
+	public HumanPlayer(String playerName, int row, int column, Color color) {
+		super(playerName, row, column, color);
+	}
+}
---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,10 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class gameActionTests {
+
+	
+}
