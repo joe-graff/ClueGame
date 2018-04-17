36dd8fb715ddf33ab4f80d2394d2684d2af7e115
Joseph Graff <jgraff@mymail.mines.edu>
2018-04-17 11:02:59 -0600

    People cards added

--- src/clueGame/MyCardsPanel.java
+++ src/clueGame/MyCardsPanel.java
@@ -1,12 +1,51 @@
 package clueGame;
 
-import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
-public class MyCardsPanel {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class MyCardsPanel extends JPanel{
 	private JTextField people, weapons, rooms;
 	private static Board board;
 	
 	public MyCardsPanel() {
-		
+		board = Board.getInstance();
+		setLayout(new GridLayout(3,0));
+		setBorder(new TitledBorder(new EtchedBorder(), "My Cards:"));
+		JPanel panel = createPeople();
+		add(panel);
+	}
+	
+	private JPanel createPeople() {
+		JPanel tempPanel = new JPanel();
+		tempPanel.setLayout(new GridLayout(1,1));
+		JLabel lable = new JLabel("People:");
+		JTextField people = new JTextField(15);
+		String card = "";
+		for(Card c : board.getPlayer(0).getHand()) {
+			if(c.cardType == CardType.PERSON)
+				card += c.getCardName();
+		}
+		people.setEditable(false);
+		people.setText(card);
+		tempPanel.add(lable);
+		tempPanel.add(people);
+		return tempPanel;
+	}
+	
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200,150);
+		MyCardsPanel panel = new MyCardsPanel();
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setVisible(true);
 	}
 }
