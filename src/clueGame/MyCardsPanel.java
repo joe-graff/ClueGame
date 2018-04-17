611ab56b39ae435a0c1ac70b9bb28d91dc19089c
Joseph Graff <jgraff@mymail.mines.edu>
2018-04-17 10:44:29 -0600

    MyCardsPanel.java added

---
+++ src/clueGame/MyCardsPanel.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+import javax.swing.JTextField;
+
+public class MyCardsPanel {
+	private JTextField people, weapons, rooms;
+	private static Board board;
+	
+	public MyCardsPanel() {
+		
+	}
+}
